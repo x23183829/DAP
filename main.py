39777ff49e80e4dbcb5ec92b89ea88c289af8ecd
haritha <x23213248@student.ncirl.ie>
2024-04-26 21:49:26 +0100

    Data loaded from MongoDB

--- main.py
+++ main.py
@@ -2,16 +2,34 @@ import pandas as pd
 import pymongo
 import json
 
-def load_data_to_mongodb(input_file):
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client["DAP"]
+
+MONGODB_URI = "mongodb://localhost:27017/"
+DB_NAME = "DAP"
+COLLECTION_NAME = "files"
+
+def load_data_to_mongodb(input_file, uri, db_name, collection_name):
+    client = pymongo.MongoClient(uri)
+    db = client[db_name]
     with open(input_file, 'r') as file:
         json_data = json.load(file)
-    collection = db["files"]
+    collection = db[collection_name]
     for doc in json_data:
         collection.insert_one(doc)
     client.close()
 
+def load_data_from_mongodb(uri, db_name, collection_name):
+    client = pymongo.MongoClient(uri)
+    db = client[db_name]
+    collection = db[collection_name]
+    cursor = collection.find({})
+    data = list(cursor)
+    df = pd.DataFrame(data)
+    client.close()
+    return df
+
 if __name__ == "__main__":
     input_file = "HPI_master.json"
-    load_data_to_mongodb(input_file)
+    #load_data_to_mongodb(input_file, MONGODB_URI, DB_NAME, COLLECTION_NAME)
+    df = load_data_from_mongodb(MONGODB_URI, DB_NAME, COLLECTION_NAME)
+    print(df.describe())
+
