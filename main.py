48901d709c956d65b1288c35264af0fb8de47be3
haritha <x23213248@student.ncirl.ie>
2024-04-28 01:53:15 +0100

    Data Cleaned

--- main.py
+++ main.py
@@ -27,9 +27,19 @@ def load_data_from_mongodb(uri, db_name, collection_name):
     client.close()
     return df
 
+def clean_data(df):
+    df.drop(columns=['index_sa'], inplace=True)
+    # df.drop_duplicates(inplace=True)
+    # df.dropna(inplace=True)
+    df.rename(columns={'period': 'months'}, inplace=True)
+    return df
+
+
 if __name__ == "__main__":
     input_file = "HPI_master.json"
     #load_data_to_mongodb(input_file, MONGODB_URI, DB_NAME, COLLECTION_NAME)
     df = load_data_from_mongodb(MONGODB_URI, DB_NAME, COLLECTION_NAME)
-    print(df.describe())
+    df = clean_data(df)
+    print(df.describe(),"\n",df.head(5))
+    
 
